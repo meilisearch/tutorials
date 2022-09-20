dcca7748f81b3a39419547fb9baf8427115e2f1b
CaroFG <carolina.ferreira131@gmail.com>
2022-09-20 17:23:26 +0200

    Add backend files

---
+++ backend/src/index.js
@@ -0,0 +1,38 @@
+const { MeiliSearch } = require('meilisearch')
+
+;(async () => {
+  try {
+    const config = {
+      host: 'http://127.0.0.1:7700'
+    };
+
+    const meili = new MeiliSearch(config);
+    
+    await meili.isHealthy()
+    await meili.createIndex('decathlon'); 
+    
+    const decathlon = require("../decathlon.json");
+    const index = await meili.index("decathlon");
+
+    const newSettings = {
+      rankingRules: [
+        "words",
+        "typo",
+        "proximity",
+        "attribute",
+        "sort",
+        "exactness",
+        "desc(creation_date)"
+      ]
+    };
+
+    await index.updateSettings(newSettings);
+    console.dir(await index.getSettings());
+    
+    await index.addDocuments(decathlon);
+        
+  } catch (e) {
+    console.error(e);
+    console.log("Meili error: ", e.message);
+  }
+})();
