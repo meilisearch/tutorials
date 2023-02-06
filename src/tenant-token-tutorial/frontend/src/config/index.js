3897e71ad1ba76664925a94cd52373d1e3cb74d0
CaroFG <carolina.ferreira131@gmail.com>
2023-02-06 16:11:25 +0100

    Update port to resolve conflict with macOS already in use port

--- src/tenant-token-tutorial/frontend/src/config/index.js
+++ src/tenant-token-tutorial/frontend/src/config/index.js
@@ -4,7 +4,7 @@ export const MEILISEARCH_CONFIG = {
   HOST: process.env.REACT_APP_MEILI_HOST || 'http://localhost:7700',
   MASTER_KEY: process.env.REACT_APP_MEILI_API_KEY || 'masterKey',
   INDEX: process.env.REACT_APP_MEILI_INDEX || 'patient_medical_record',
-  API_HOST: process.env.REACT_APP_API_HOST || 'http://localhost:5000',
+  API_HOST: process.env.REACT_APP_API_HOST || 'http://localhost:5001',
 }
 
 const apiHost = MEILISEARCH_CONFIG.API_HOST
