d6a991651e4ab90ee081036e0445ceb571244068
CaroFG <carolina.ferreira131@gmail.com>
2023-03-22 10:01:28 +0100

    Update meilisearch host

--- src/react-decathlon/backend/src/steps/delete_index.js
+++ src/react-decathlon/backend/src/steps/delete_index.js
@@ -3,7 +3,7 @@ const { MeiliSearch } = require('meilisearch')
 ;(async () => {
   try {
     const config = {
-      host: 'http://127.0.0.1:7700'
+      host: 'http://localhost:7700'
     }
 
     const meili = new MeiliSearch(config)
